b5fc2812967e3bfaf659729faa861f352b137da8
Prathamesh094 <prathamesh.howale@gmail.com>
2022-07-22 17:13:32 +0530

    array operations in js all covered

---
+++ arr.js
@@ -0,0 +1,57 @@
+   //  let no=[10,20,30,40,50,60]
+   //  document.write(no,"<br>")
+   //  var b=no.length
+   //  document.write("length of the given array is :",b,"<br>");
+
+   // //  document.write(no[no.length-1],"<br>");
+   //  let arr1=[];
+   //  for(i=0;i<no.length;i++){
+   //     arr1.push(no[i]);
+   //  }
+   
+   //  document.write("this is new array",arr1)
+
+   // var arr1=[10,20,30,40]
+   // document.write(arr1);
+   // var arr1=[,50,60]
+   // document.write(arr1);
+   // we cn update array likewise if use var  it wont work if we use const keyword.
+
+
+
+   // fruits2=[];       while llop in array
+
+   // let i=0;
+   // while(i<fruits.length){
+   //    fruits2.push(fruits[i])
+   //    i++
+   // }
+   
+   // const fruits = ["apple","mango","grapes"];
+   // const fruits2=[];
+   // // for(let fruit of fruits){
+   // //    document.write(fruit,"<br>");
+   // //    console.log(fruit) 
+   // //    fruits2.push(fruit.toUpperCase());   
+   // // }
+   // // document.write(fruits2,"<br>");
+   // document.write(fruits,"<br>")
+   // for(let index in fruits){
+      
+   //     fruits2.push(fruits[index].toUpperCase())
+
+   // }
+   // document.write(fruits2)
+
+
+   //********************arra distructuring */
+
+   const myarr=["value1","value2","value3","value4"]
+
+   let [myvar1,,myvar2]=myarr;
+   document.write("value of myvar1 :",myvar1)
+   document.write("<br>value of myvar2 : ",myvar2)
+
+
+
+  
